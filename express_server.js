da2ee2db872a41d4bc43730e2a4f797614d55913
vagrant <Juelz Lum>
2023-01-24 04:45:01 +0000

    additional adjustment to the url

--- express_server.js
+++ express_server.js
@@ -27,12 +27,12 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.get("/urls" , (req, res ) => {
+app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
-})
+});
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL:urlDatabase  };
+  const templateVars = { id: req.params.id, longURL: urlDatabase };
   res.render("urls_show", templateVars);
 });
