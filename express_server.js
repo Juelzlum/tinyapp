3203800117fbec45698db76ce285ddbaba8db189
vagrant <Juelz Lum>
2023-01-26 01:36:50 +0000

    created show and update index

--- express_server.js
+++ express_server.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const cookieParser = require('cookie-parser')
 
 app.set("view engine", "ejs");
+app.use(cookieParser())// populates req.cookies
+app.use(express.urlencoded({extended: false})) //populataes req.body
 
+//data base 
+const user = {
+  abc: {
+  'id' : 'jul',
+  'email' : 'jul@com',
+  'password' : '1234'}
+}
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -12,7 +22,14 @@ const urlDatabase = {
 };
 
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.send("Welcome To TinyApp");
+});
+
+app.get("/login", (req, res) => {
+  // const templateVars = {
+  //   user: user[req.session["userID"]]
+  // };
+  // res.render("urls_login", templateVars);
 });
 
 app.listen(PORT, () => {
@@ -36,14 +53,18 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase };
-  urlDatabase[req.params.id].longURL = longURL;
-  res.redirect("/url");
+  const id = req.params.id
+  // console.log('id:', id)
+  const longUrl = urlDatabase[id]
+  // console.log('longUrl', longUrl)
+  const templateVars = {id: id, longUrl: longUrl}
+  // res.redirect("/url");
   res.render("urls_show", templateVars);
 });
 
-app.use(express.urlencoded({ extended: true }));
+
 
 function generateRandomString() { }
 
@@ -61,5 +82,11 @@ app.post("/urls/:shortURL/delete", (req,res) => {
   res.redirect("/urls");
 })
 
+app.post('/login' ,(req,res) => {
+  const username = req.body.username
+  const password = req.body.password
+  res.cookie(username,password)
+  res.redirect("/urls");
+})
 
 
