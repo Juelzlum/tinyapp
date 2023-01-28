5b363252a202ae4b6cd5bb045e4e26e8815d4b3e
vagrant <Juelz Lum>
2023-01-28 21:46:42 +0000

    added a function.js file to clean up express server

---
+++ function.js
@@ -0,0 +1,54 @@
+const findEmail = (email, db) => {
+  for (let key in db) {
+    if (email === db[key].email) {
+      return email;
+    }
+  }
+  return undefined;
+};
+
+const findUserId = (email, db) => {
+  for (const user in db) {
+    if (db[user].email === email) {
+      return db[user].id;
+    }
+  }
+};
+
+//check to see if password exist
+const findPassword = (email, db) => {
+  for (let key in db) {
+    if (email === db[key].email) {
+      return db[key].password;
+    }
+  }
+  return undefined;
+};
+
+
+const generateRandomString = () => {
+  const alphaNumerical = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let result = '';
+  for (let i = 0; i < 6; i++) {
+    result += alphaNumerical.charAt(Math.floor(Math.random() * alphaNumerical.length));
+  }
+  return result;
+};
+
+const urlsForUser = function(id, urlDatabase) {
+  const userUrls = {};
+  for (const shortURL in urlDatabase) {
+    if (urlDatabase[shortURL].userID === id) {
+      userUrls[shortURL] = urlDatabase[shortURL];
+    }
+  }
+  return userUrls;
+};
+
+module.exports = {
+  generateRandomString,
+  findEmail,
+  findUserId,
+  findPassword,
+  urlsForUser
+};
